c5d315624539a56590814c0a7e43459ed9d2bea4
Pedro Parente <pedroparente@seegno.com>
2024-04-12 16:13:29 +0100

    Add two factor methods to resolver

--- src/auth/managers/auth-manager.js
+++ src/auth/managers/auth-manager.js
@@ -3,6 +3,7 @@
  * Module dependencies.
  */
 
+import { merge } from 'lodash';
 import AbstractManager from 'core/managers/abstract-manager';
 import UserModel from 'user/models/user-model';
 
@@ -11,6 +12,66 @@ import UserModel from 'user/models/user-model';
  */
 
 export default class AuthManager extends AbstractManager {
+
+  /**
+   * Confirm two factor code.
+   */
+
+  async confirmTwoFactorCode(twoFactorId, data) {
+    const { data: token } = await this.resolver.confirmTwoFactorCode(merge({}, data, { twoFactorId }));
+
+    return token;
+  }
+
+  /**
+   * Disable two factor.
+   */
+
+  async disableTwoFactor(userId, data) {
+    await this.resolver.disableTwoFactor(merge({}, data, { userId }));
+
+    return true;
+  }
+
+  /**
+   * Enable two factor.
+   */
+
+  async enableTwoFactor(userId, data) {
+    const { data: recoveryCodes } = await this.resolver.enableTwoFactor(merge({}, data, { userId }));
+
+    return recoveryCodes;
+  }
+
+  /**
+   * Generate two factor recovery codes.
+   */
+
+  async generateTwoFactorRecoveryCodes(userId) {
+    const { data: recoveryCodes } = await this.resolver.generateTwoFactorRecoveryCodes({ userId });
+
+    return recoveryCodes;
+  }
+
+  /**
+   * Get two factor recovery codes.
+   */
+
+  async getTwoFactorRecoveryCodes(userId) {
+    const { data: recoveryCodes } = await this.resolver.getTwoFactorRecoveryCodes({ userId });
+
+    return recoveryCodes;
+  }
+
+  /**
+   * Get two factor secret.
+   */
+
+  async getTwoFactorSecret() {
+    const { data: secret } = await this.resolver.getTwoFactorSecret();
+
+    return secret;
+  }
 
   /**
    * Login.
@@ -40,6 +101,16 @@ export default class AuthManager extends AbstractManager {
     const { data: auth } = await this.resolver.refresh(data);
 
     return this.instantiate(auth);
+  }
+
+  /**
+   * Start two factor flow.
+   */
+
+  async startTwoFactorFlow(data) {
+    const { data: twoFactorId } = await this.resolver.startTwoFactorFlow(data);
+
+    return twoFactorId;
   }
 
   /**
