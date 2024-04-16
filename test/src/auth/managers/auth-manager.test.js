f65cbf2a8e6cf9bdeed99b1a49f06eb7606eeff8
Pedro Parente <pedroparente@seegno.com>
2024-04-16 17:17:06 +0100

    Add two factor methods unit tests

--- test/src/auth/managers/auth-manager.test.js
+++ test/src/auth/managers/auth-manager.test.js
@@ -15,6 +15,78 @@ describe('AuthManager', () => {
   const host = 'http://foobar:3000';
   const slyk = sdk({ apikey, host });
 
+  describe('confirmTwoFactorCode', () => {
+    it('should call api `/users/auth/2fa/foo/confirm` endpoint with method `post` and return a `trust token`', async () => {
+      nock(host, { reqheaders: { apikey } })
+        .post('/users/auth/2fa/foo/confirm', { code: '123' })
+        .reply(200, { data: { trustToken: '123' } });
+
+      const result = await slyk.auth.confirmTwoFactorCode('foo', { code: '123' });
+
+      expect(result).toEqual({ trustToken: '123' });
+    });
+  });
+
+  describe('disableTwoFactor', () => {
+    it('should call api `/users/:userId/auth/2fa` endpoint with method `delete` and return `true`', async () => {
+      nock(host, { reqheaders: { apikey } })
+        .delete('/users/foo/auth/2fa', { code: '123', methodId: 'bar' })
+        .reply(204);
+
+      const result = await slyk.auth.disableTwoFactor('foo', { code: '123', methodId: 'bar' });
+
+      expect(result).toEqual(true);
+    });
+  });
+
+  describe('enableTwoFactor', () => {
+    it('should call api `/users/:userId/auth/2fa` endpoint with method `post` and return a list of `recoveryCodes`', async () => {
+      nock(host, { reqheaders: { apikey } })
+        .post('/users/foo/auth/2fa', { code: '123', secret: 'bar' })
+        .reply(200, { data: { recoveryCodes: ['123', '456'] } });
+
+      const result = await slyk.auth.enableTwoFactor('foo', { code: '123', secret: 'bar' });
+
+      expect(result).toEqual({ recoveryCodes: ['123', '456'] });
+    });
+  });
+
+  describe('generateTwoFactorRecoveryCodes', () => {
+    it('should call api `/users/:userId/auth/2fa/recovery-codes` endpoint with method `post` and return a list of `recoveryCodes`', async () => {
+      nock(host, { reqheaders: { apikey } })
+        .post('/users/foo/auth/2fa/recovery-codes')
+        .reply(200, { data: { recoveryCodes: ['123', '456'] } });
+
+      const result = await slyk.auth.generateTwoFactorRecoveryCodes('foo');
+
+      expect(result).toEqual({ recoveryCodes: ['123', '456'] });
+    });
+  });
+
+  describe('getTwoFactorRecoveryCodes', () => {
+    it('should call api `/users/:userId/auth/2fa/recovery-codes` endpoint with method `get` and return a list of `recoveryCodes`', async () => {
+      nock(host, { reqheaders: { apikey } })
+        .get('/users/foo/auth/2fa/recovery-codes')
+        .reply(200, { data: { recoveryCodes: ['123', '456'] } });
+
+      const result = await slyk.auth.getTwoFactorRecoveryCodes('foo');
+
+      expect(result).toEqual({ recoveryCodes: ['123', '456'] });
+    });
+  });
+
+  describe('getTwoFactorSecret', () => {
+    it('should call api `/users/auth/2fa/secret` endpoint with method `get` and return a two factor `secret`', async () => {
+      nock(host, { reqheaders: { apikey } })
+        .get('/users/auth/2fa/secret')
+        .reply(200, { data: { secret: '123', secretBase32Encoded: '456' } });
+
+      const result = await slyk.auth.getTwoFactorSecret();
+
+      expect(result).toEqual({ secret: '123', secretBase32Encoded: '456' });
+    });
+  });
+
   describe('login', () => {
     it('should call api `/auth/token` endpoint with method `post` and return an instance of `Auth` model with the given `data`', async () => {
       nock(host, { reqheaders: { apikey } })
@@ -33,6 +105,22 @@ describe('AuthManager', () => {
         refreshToken: '123',
         roles: ['user'],
         token: '456'
+      });
+    });
+
+    it('should call api `/auth/token` endpoint with method `post` and return an instance of `Auth` model with the given `data` if two factor is enabled', async () => {
+      nock(host, { reqheaders: { apikey } })
+        .post('/auth/token', { email: 'foo@bar.com', password: 'Foobar123' })
+        .reply(242, {
+          data: {
+            twoFactorId: 'foo'
+          }
+        });
+
+      const result = await slyk.auth.login({ email: 'foo@bar.com', password: 'Foobar123' });
+
+      expect(result).toEqual({
+        twoFactorId: 'foo'
       });
     });
   });
@@ -63,6 +151,18 @@ describe('AuthManager', () => {
       const result = await slyk.auth.refresh({ refreshToken: '123' });
 
       expect(result).toEqual({ refreshToken: '123', token: '456' });
+    });
+  });
+
+  describe('startTwoFactorFlow', () => {
+    it('should call api `/users/auth/2fa/start` endpoint with method `post` and return a `twoFactorId`', async () => {
+      nock(host, { reqheaders: { apikey } })
+        .post('/users/auth/2fa/start', { email: 'foo@bar.com', password: 'Foobar123' })
+        .reply(200, { data: { twoFactorId: 'foo' } });
+
+      const result = await slyk.auth.startTwoFactorFlow({ email: 'foo@bar.com', password: 'Foobar123' });
+
+      expect(result).toEqual({ twoFactorId: 'foo' });
     });
   });
 
